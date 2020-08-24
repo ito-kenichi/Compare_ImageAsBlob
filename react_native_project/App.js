a2e9df6eb334821d472a71cdb369c4fc356768c7
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-08-24 20:25:33 +0900

    WIP React Native

--- react_native_project/App.js
+++ react_native_project/App.js
@@ -1,14 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as SQLite from 'expo-sqlite';
+// import * as Asset from 'expo-asset'
+import { Asset } from "expo-asset";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+var db;
+
+export default class App extends Component {
+
+  state = {
+    text: null,
+    downloaded: false,
+  };
+
+  downloadDatabase = async () => {
+  
+    try {
+      const sqliteDirectory = `${FileSystem.documentDirectory}`;
+  
+      const { exists, isDirectory } = await FileSystem.getInfoAsync(
+        sqliteDirectory
+      );
+
+      var sqliDir = false;
+      if (!exists) {
+        sqliDir = await FileSystem.makeDirectoryAsync(`${sqliteDirectory}`);
+      } else if (!isDirectory) {
+        throw new Error('SQLite dir is not a directory');
+      }
+
+      const pathToDownloadTo = `${sqliteDirectory}assets/data.db`;
+      const uriToDownload = Asset.fromModule(require('./assets/data.db')).uri;
+    
+      // console.log((await FileSystem.getInfoAsync(`${sqliteDirectory}assets/data.db`)).exists);
+      await FileSystem.downloadAsync(uriToDownload, `${sqliteDirectory}assets/data.db`);
+      
+    } catch(error) {
+        console.log(error);
+    }
+  }
+  
+  componentDidMount() {
+
+    this.downloadDatabase();
+
+    db = SQLite.openDatabase('./assets/data.db');
+    console.log("db : " + db);
+    db.transaction(tx => {
+      try {
+        
+        // tx.executeSql("select * from places");
+          tx.executeSql(
+          "select name, image from places;",
+          (_, resultSet) => {
+             this.setState({
+               text: resultSet.map(x => x.name),
+             })
+            // SUCCESS
+            setItems(resultSet);
+            console.log("resultSet : " + resultSet);
+          },
+        );
+      } catch(error) {
+        console.log(error);
+      }
+    });
+  };
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.text}</Text>
+        <Text>Hello World</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
